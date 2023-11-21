cb6c1530741c2dbc65495976a1725a2dea91a32f
Elena Zen <elenazen563@gmail.com>
2023-11-21 14:37:17 +0100

    hsm - responsive rooms page

--- hotel sogno in maremma/script.js
+++ hotel sogno in maremma/script.js
@@ -7,12 +7,31 @@ function show() {
         menu.classList.toggle("openmenu");
     }
 }
-
+function checkIfMobile() {
+    // Utilizza la stessa condizione per determinare se il dispositivo è mobile
+    return window.matchMedia("(max-width: 500px)").matches;
+  }
 document.addEventListener('DOMContentLoaded', function () {
-    const roomOptions = document.querySelectorAll('.room-option');
+    const roomOptions = document.querySelectorAll('.room-option.desktop');
     const roomGalleries = document.querySelectorAll('.room_gallery');
+    let isMobile = checkIfMobile();
 
     roomOptions.forEach(function (roomOption, index) {
+        
+        if (roomOption.classList.contains('comfort')) {
+            if (isMobile) {
+                roomOption = document.querySelector('.room-option.comfort.mobile');
+            } else {
+                roomOption = document.querySelector('.room-option.comfort.desktop');
+            }
+        }
+        if (roomOption.classList.contains('vasca')) {
+            if (isMobile) {
+                roomOption = document.querySelector('.room-option.vasca.mobile');
+            } else {
+                roomOption = document.querySelector('.room-option.vasca.desktop');
+            }
+        }
         roomOption.addEventListener('click', function () {
             // Nascondi tutte le room_gallery tranne quella corrente
             roomGalleries.forEach(function (roomGallery, galleryIndex) {
